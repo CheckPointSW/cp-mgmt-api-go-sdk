d865a842f0cfab9b04351d461a40c2e85f115eb7
chkp-royl <royl@checkpoint.com>
2023-05-11 23:02:40 +0300

    Auto publish feature

--- Examples/show_hosts.go
+++ Examples/show_hosts.go
@@ -1,7 +1,7 @@
 package Examples
 
 import (
-	api "../APIFiles"
+	api "cp-mgmt-api-go-sdk/APIFiles"
 	"fmt"
 	"os"
 )
@@ -21,7 +21,7 @@ func ShowHosts() {
 	fmt.Printf("Enter password: ")
 	fmt.Scanln(&password)
 
-	args := api.APIClientArgs(api.DefaultPort, "", "", apiServer, "", -1, "", false, false, "deb.txt", api.WebContext, api.TimeOut, api.SleepTime, "", "")
+	args := api.APIClientArgs(api.DefaultPort, "", "", apiServer, "", -1, "", false, false, "deb.txt", api.WebContext, api.TimeOut, api.SleepTime, "", "", -1)
 
 	client := api.APIClient(args)
 
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"./Examples"
+	Examples "cp-mgmt-api-go-sdk/Examples"
 	"fmt"
 	"os"
 )
 
 func main(){
+	if len(os.Args) < 2 {
+		fmt.Println("Operation not found. Optional operations are rule/discard/add_host/show_hosts/dup_ip/auto_publish")
+		os.Exit(0)
+	}
 
 	switch os.Args[1] {
 	case "discard":
@@ -19,7 +23,9 @@ func main(){
 		Examples.ShowHosts()
 	case "dup_ip":
 		Examples.DupIp()
+	case "auto_publish":
+		Examples.AutoPublish()
 	default:
-		fmt.Println("Operation not supported. Optional operations are rule/discard/add_host/show_hosts/dup_ip")
+		fmt.Println("Operation not supported. Optional operations are rule/discard/add_host/show_hosts/dup_ip/auto_publish")
 	}
 }
